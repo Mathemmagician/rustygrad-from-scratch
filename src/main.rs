8ce1f58c846dd97d1bca6569fb81a5a9ec420a57
Mathemmagician <ramil9898@gmail.com>
2023-03-22 13:50:05 -0400

    rc refcell

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use std::{
+    cell::RefCell,
+    rc::Rc,
+};
 
-struct Value {
+#[derive(Clone)]
+struct Value(Rc<RefCell<ValueData>>);
+
+struct ValueData {
     data: f64,
     grad: f64,
     _prev: Vec<Value>,
-    _backward: Option<fn(value: &Value)>,
+    _backward: Option<fn(value: &ValueData)>,
 }
 
-impl Value {
-    fn new(data: f64) -> Value {
-        Value {
+impl ValueData {
+    fn new(data: f64) -> ValueData {
+        ValueData {
             data,
             grad: 0.0,
             _prev: Vec::new(),
@@ -17,6 +24,22 @@ impl Value {
     }
 }
 
-fn main() {
+impl Value {
+    fn new(value: ValueData) -> Value {
+        Value(Rc::new(RefCell::new(value)))
+    }
+}
 
+impl<T: Into<f64>> From<T> for Value {
+    fn from(t: T) -> Value {
+        Value::new(ValueData::new(t.into()))
+    }
+}
+
+fn main() {
+    let a = Value::from(-4.0);
+    println!("data = {}", a.0.borrow().data);  // data = -4
+
+    a.0.borrow_mut().data = 7.5;
+    println!("data = {}", a.0.borrow().data);  // data = 7.5
 }
